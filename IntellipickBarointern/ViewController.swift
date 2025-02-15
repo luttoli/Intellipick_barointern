17b702f628543fc66c1391ca6ab307386df7a2a2
luttoli <hhn0212@naver.com>
2025-02-15 13:28:47 +0900

    Initial Commit

---
+++ IntellipickBarointern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntellipickBarointern
+//
+//  Created by 김지훈 on 2/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
